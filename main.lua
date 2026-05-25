4d2e19da1d642540f5786f16d1cb2685168e1387
HonestParley27 <dhanvithc@hotmail.com>
2026-05-25 10:20:12 +1000

    Reordering of Mod Struct
    Moved Custom Objects to Items, and Libs is now a library for custom global functions or other implementations
    
    Adjusted Main.lua to fit new loading sequence and functionality

--- main.lua
+++ main.lua
@@ -1,9 +1,22 @@
 local modPath = "".. SMODS.current_mod.path
 
-local files = NFS.getDirectoryItems(modPath.. "lib")
--- All Added Content should be within the lib folder, to ensure it's loaded.
 
+-- All Added Content should be within the two below specified folder, to ensure it's loaded.
+
+if not BPMadness then
+    BPMadness = {}
+end
+
+-- Load all Items :3
+local files = NFS.getDirectoryItems(modPath.."items")
 for _,file in ipairs(files) do
 	print("[BluePir Madness] Loading file " .. file)
-    assert(SMODS.load_file("lib/"..file))()
+    assert(SMODS.load_file("items/"..file))()
+end
+
+-- Load all silly functions
+files = NFS.getDirectoryItems(modPath.."libs")
+for _,file in ipairs(files) do
+    print("[BluePir Madness] Loading file" .. file)
+    assert(SMODS.load_file("libs/"..file))()
 end
